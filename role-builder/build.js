7149135d4281b5a2f081e8134a9313b4bf3fc617
Mark Huot <mark@markhuot.com>
2015-01-31 02:56:16 -0500

    more dynamic and less debugging

--- role-builder/build.js
+++ role-builder/build.js
@@ -6,7 +6,7 @@ connection.on('ready', function () {
   connection.queue('to-build', function (q) {
       q.bind('#');
       q.subscribe(function (message) {
-        spawn('./git-clone /vagrant/debug/repo.git HEAD | ./docker-build repo master').stdout.on('data', function(chunk) {
+        spawn('./build /vagrant/debug/repo.git HEAD repo master').stdout.on('data', function(chunk) {
           console.log(chunk.toString());
         }).stderr.on('data', function(chunk) {
           console.log(chunk.toString());
